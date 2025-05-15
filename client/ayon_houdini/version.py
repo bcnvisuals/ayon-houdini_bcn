04d2a46de794242f3741e5e30514cfb79d7b991d
George Antonopoulos <george.antonopoulos@bcnvisuals.com>
2025-05-15 09:24:19 +0000

    version

--- client/ayon_houdini/version.py
+++ client/ayon_houdini/version.py
@@ -1,7 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring AYON addon 'houdini' version."""
-<<<<<<< HEAD
-__version__ = "0.4.1-bcn.1"
-=======
-__version__ = "0.4.5"
->>>>>>> 0.4.5
+__version__ = "0.4.5-bcn.1"
